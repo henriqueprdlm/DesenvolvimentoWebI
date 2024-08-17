45cc9c1b2a36c5bb40c776246f5d51471c09429b
Henrique Pieri de Lima <henrique070425@gmail.com>
2024-08-16 23:05:04 -0300

    Atividades introdutórias a HTML, CSS e JS

---
+++ BeginnerMistakes/js/main.js
@@ -0,0 +1,11 @@
+const mensagem = "Olá, mundo!";
+const posts = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .catch(err => console.log(err));
+}
+
+export {mensagem, posts};
+function mostrarDados() {
+    console.log('Teste');
+}
